4db4e87905e6eef1b7807d54ff9db59716d2c696
Joey Liu <nantsouliu@microsoft.com>
2025-12-18 23:46:30 +0000

    upgrade to v1.27.3 and refactoring

--- temporal/common/persistence/sql/sqlplugin/db_handle.go
+++ temporal/common/persistence/sql/sqlplugin/db_handle.go
@@ -63,6 +63,9 @@ type DatabaseHandle struct {
 	timeSource  clock.TimeSource
 	// Ensures only one refresh call happens at a time
 	sync.Mutex
+
+	// msft: For background heartbeat
+	cancel context.CancelFunc
 }
 
 // An invalid connection returns `DatabaseUnavailableError` for all operations
@@ -75,6 +78,7 @@ func NewDatabaseHandle(
 	metricsHandler metrics.Handler,
 	timeSource clock.TimeSource,
 ) *DatabaseHandle {
+	ctx, cancel := context.WithCancel(context.Background()) // msft
 	handle := &DatabaseHandle{
 		running:      true,
 		connect:      connect,
@@ -82,8 +86,10 @@ func NewDatabaseHandle(
 		metrics:      metricsHandler,
 		logger:       logger,
 		timeSource:   timeSource,
+		cancel:       cancel, // msft
 	}
 	handle.reconnect(true)
+	go handle.heartbeat(ctx) // msft
 	return handle
 }
 
@@ -141,6 +147,7 @@ func (h *DatabaseHandle) Close() {
 
 	if h.running {
 		h.running = false
+		h.cancel()
 		db := h.db.Swap(nil)
 		if db != nil {
 			db.Close()
@@ -160,18 +167,12 @@ func (h *DatabaseHandle) DB() (*sqlx.DB, error) {
 }
 
 func (h *DatabaseHandle) Conn() Conn {
-	if db := h.db.Load(); db != nil {
-		return db
-	}
-
-	if db := h.reconnect(false); db != nil {
-		return db
-	}
-	return invalidConn{}
+	return &tokenAuthConn{h: h}
 }
 
 func (h *DatabaseHandle) ConvertError(err error) error {
 	if h.needsRefresh(err) ||
+		IsAccessTokenExpiredError(err) || // msft
 		errors.Is(err, driver.ErrBadConn) ||
 		errors.Is(err, io.ErrUnexpectedEOF) ||
 		errors.Is(err, io.EOF) ||
